cb225557872116f81423847447986cc86fd068be
Marco Brandizi <brandizi@ebi.ac.uk>
2012-12-03 15:38:59 +0000

    New configuration mechanism for the web service configured. Preparing version 0.0.1.

---
+++ myequivalents-wscli/src/test/java/uk/ac/ebi/fg/myequivalents/webservices/client/MyServerWSTest.java
@@ -0,0 +1,39 @@
+/*
+ * 
+ */
+package uk.ac.ebi.fg.myequivalents.webservices.client;
+
+import static java.lang.System.out;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import uk.ac.ebi.fg.myequivalents.managers.interfaces.EntityMappingManager;
+import uk.ac.ebi.fg.myequivalents.managers.interfaces.EntityMappingSearchResult;
+
+/**
+ * A dirty/quick test for a new server of the web service. 
+ * 
+ * <dl><dt>date</dt><dd>Dec 3, 2012</dd></dl>
+ * @author Marco Brandizi
+ *
+ */
+public class MyServerWSTest
+{
+	@Test @Ignore ( "This is a sort of quick/dirty manual test against your new web service instance. Usually disabled" )
+	public void testGetMapping ()
+	{
+		EntityMappingManager mmgr = new EntityMappingWSClient ( "http://localhost:21480/myequivalents/ws" );
+		EntityMappingSearchResult result = mmgr.getMappings ( false, "biosamples-service:SAMEA1006750" );
+		String resultStr = result.toString ();
+		
+		out.println ( "\n\n ====================== '/mapping/get' says:\n" + resultStr + "============================" );
+		
+		assertNotNull ( "'mapping get' didn't work!", result );
+		assertTrue ( "Wrong result from 'mapping get' (ena-repository/DRS000019)!", 
+			resultStr.toString ().contains ( "ena-repository" ) && resultStr.contains ( "DRS000019" )
+		);
+	}
+}
