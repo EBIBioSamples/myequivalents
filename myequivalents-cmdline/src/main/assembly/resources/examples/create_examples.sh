a2e5d3fb9b6fb5e489a9079b1d2619fac06fa8bf
Marco Brandizi <brandizi@ebi.ac.uk>
2015-08-25 22:44:41 +0100

    Initialisation script and bin rearrangement. More debugging. Comments.

--- myequivalents-cmdline/src/main/assembly/resources/examples/create_examples.sh
+++ myequivalents-cmdline/src/main/assembly/resources/examples/create_examples.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 cd "$(dirname $0)/.."
-./myeq.sh user store -y examples/admin_user.xml
+echo "\n\n\n\tGoing to create examples, This requires prior run of init.sh\n\n"
+
+set -o xtrace
 ./myeq.sh service store -u admin -s admin.secret <examples/foo_services.xml
 ./myeq.sh mapping store -u admin -s admin.secret test.testmain.service6:acc1 test.testmain.service7:acc2
 ./myeq.sh mapping store-bundle -u admin -s admin.secret test.testmain.service6:acc1 test.testmain.service7:acc3 test.testmain.service8:acc1
 ./myeq.sh mapping store-bundle -u admin -s admin.secret test.testmain.service6:acc10 test.testmain.service7:acc20 test.testmain.service8:acc30
-./myeq.sh mapping get -u admin -s admin.secret test.testmain.service7:acc20
+
+# URIs are used to refer entities. In the first case the service the entity is about is given
+# Second case doen't mention the service, it will be retrieved by matching the URI with URI pattern (slightly slower)
+# Last case says that the URI is not associated to any service, which myEq transaltes into the special 'unspecified'
+#   service ( '_:<uri>' is an equivalent form, where the unspecified service name '_' is explicitly used) 
+./myeq.sh mapping store-bundle -u admin -s admin.secret \
+	'test.testmain.service6:<http://somewhere.in.the.net/testmain/service6/someType1/uritest1>' \
+	'<http://somewhere.in.the.net/testmain/service6/someType1/uritest2>' \
+	':<http://www.you.cannnot.find.me/testmain/uri3>'
+
+
+./myeq.sh mapping get test.testmain.service7:acc20
+
+# URIs associated to services are broken into the form service:acc
+./myeq.sh mapping get 'test.testmain.service6:uritest1'
+
+# But you can still use them
+./myeq.sh mapping get '<http://somewhere.in.the.net/testmain/service6/someType1/uritest2>'
+
+set +o xtrace
+
+printf "\n\n\n\tDone!\n\n"
---
+++ myequivalents-cmdline/src/main/assembly/resources/init.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+cd "$(dirname $0)"
+cfg_file=myeq-manager-config.xml
+
+cat <<EOT
+
+
+	Going to initialise myEquivalents relational DB
+	Please ensure you've correct settings in $cfg_file
+
+	This possibly includes the line '<prop key = "hibernate.hbm2ddl.auto"></prop>'
+	temporarily set with the value (insde ><) create or update (see [1]).
+	
+	---> Please be aware that this option might wipe out your existing database.
+	---> Morever, it's VERY important that you set it back to empty after this initialisation. 
+
+	[1] http://docs.jboss.org/hibernate/stable/orm/manual/en-US/html/ch03.html#configuration-misc-properties
+
+EOT
+
+./myeq.sh user store -y init_data/admin_user.xml
+./myeq.sh service store -u admin -s admin.secret init_data/unspecified_service.xml
+
+cat <<EOT
+
+
+	Done. ---> Remeber to change the admin password, if not already done in init_data/admin_user.xml
+
+EOT
